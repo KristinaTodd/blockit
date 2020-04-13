ca1ffe531e786bdf61a46afc02027f17af2cf1ec
Kristina Todd <Kristina.Todd22@gmail.com>
2020-04-12 20:56:09 -0600

    deployment set up

--- Startup.cs
+++ Startup.cs
@@ -48,7 +48,8 @@ namespace Keepr
                 builder
                           .WithOrigins(new string[]{
                             "http://localhost:8080",
-                            "http://localhost:8081"
+                            "http://localhost:8081",
+                            "https://block-r.herokuapp.com/"
                       })
                           .AllowAnyMethod()
                           .AllowAnyHeader()
